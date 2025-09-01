88d0b2011a7b50bda8ad6e7e8aa078c561bea42c
jimbookshkust <khhungac@connect.ust.hk>
2025-09-01 18:21:44 +0800

    Initial Commit

---
+++ RealityKit-Drawable/RealityKit_DrawableApp.swift
@@ -0,0 +1,18 @@
+//
+//  RealityKit_DrawableApp.swift
+//  RealityKit-Drawable
+//
+//  Created by jimbook on 1/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct RealityKit_DrawableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
---
+++ RealityKit-DrawableTests/RealityKit_DrawableTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealityKit_DrawableTests.swift
+//  RealityKit-DrawableTests
+//
+//  Created by jimbook on 1/9/2025.
+//
+
+import Testing
+@testable import RealityKit_Drawable
+
+struct RealityKit_DrawableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
